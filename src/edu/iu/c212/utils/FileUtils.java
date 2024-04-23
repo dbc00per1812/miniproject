e8ff7206c62c09d026584d6a0e5c10961926b9f3
Oliver Eichenberg <eichenbergoliver@gmail.com>
2024-04-22 21:33:28 -0400

    Finished majority of fileutils

--- src/edu/iu/c212/utils/FileUtils.java
+++ src/edu/iu/c212/utils/FileUtils.java
@@ -3,6 +3,8 @@ package edu.iu.c212.utils;
 import edu.iu.c212.models.*;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
 import java.io.IOException;
 import java.util.List;
 
@@ -19,12 +21,35 @@ public class FileUtils {
         System.out.println(inventoryFile/*.toURI()*/.getPath() + "\n" + inventoryFile.exists());
         // depending on your OS, toURI() may need to be used when working with paths
         // TODO
-        return null;
+        List<Item> itemList = new ArrayList<>();
+        try (Scanner scanner = new Scanner(inventoryFile)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                Item item = parseItem(line);
+                itemList.add(item);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+            System.exit(1);
+        }
+        return itemList;
     }
+
 
     public List<Staff> readStaffFromFile() throws IOException {
         // TODO
-        return null;
+        List<Staff> staffList = new ArrayList<>();
+        try (Scanner scanner = new Scanner(staffFile)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                Staff staff = parseStaff(line);
+                staffList.add(staff);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+            System.exit(1);
+        }
+        return staffList;
     }
 
     public void writeInventoryToFile(List<Item> items) {
@@ -38,7 +63,17 @@ public class FileUtils {
 
     public static List<String> readCommandsFromFile() throws IOException {
         // TODO
-        return null;
+        List<String> commands = new ArrayList<>();
+        try (Scanner scanner = new Scanner(inputFile)) {
+            while (scanner.hasNextLine()) {
+                String command = scanner.nextLine();
+                commands.add(command);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+            System.exit(1);
+        }
+        return commands;
     }
 
     public static void writeStoreScheduleToFile(List<String> lines) {
@@ -47,6 +82,24 @@ public class FileUtils {
 
     public static void writeLineToOutputFile(String line) {
         // TODO
+    }
+
+    private static Item parseItem(String s) {
+        String[] line = s.split(",");
+        String name = line[0].trim();
+        int cost = Integer.parseInt(line[1].trim());
+        int quantity = Integer.parseInt(line[2].trim());
+        int aisle = Integer.parseInt(line[3].trim());
+        return new Item(name, cost, quantity, aisle);
+    }
+
+    private static Staff parseStaff(String s) {
+        String[] line = s.split(",");
+        String name = line[0].trim();
+        int age = Integer.parseInt(line[1].trim());
+        String role = line[2].trim();
+        String availability = line[3].trim();
+        return new Staff(name, age, role, availability);
     }
 
 }
